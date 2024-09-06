9530910150aa41a83127ab62f4e332b60db5a10b
Magnus Nilsson <regnhundar@gmail.com>
2024-09-06 11:46:26 +0200

    first commit

---
+++ functions/GetRooms/index.js
@@ -0,0 +1,8 @@
+export const handler = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Go Serverless v4! Your function executed successfully!",
+    }),
+  };
+};

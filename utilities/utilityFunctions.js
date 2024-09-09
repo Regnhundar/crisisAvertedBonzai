61922dd26a842664d024780b77a688c7a4af4f5c
Magnus Nilsson <regnhundar@gmail.com>
2024-09-09 15:18:23 +0200

    start av updateAvailability

--- utilities/utilityFunctions.js
+++ utilities/utilityFunctions.js
@@ -1,11 +1,12 @@
+import db from "../../services/db.js";
 const bizLogic = (body, itemToUpdate) => {
     const error = new Error();
 
     const numberOfBeds = body.single + body.double * 2 + body.suite * 3;
 
     if (body.guests > numberOfBeds) {
-        error.msg = "Guests can't be a higher number then beds.";
-        throw error.msg;
+        error.message = "Guests can't be a higher number then beds.";
+        throw error;
     }
 
     const singlePrice = 500 * body.single;
@@ -22,4 +23,53 @@ const bizLogic = (body, itemToUpdate) => {
     return costOfStay;
 };
 
+const updateAvailability = async (rooms, oldItem) => {
+    let freeSingleRooms;
+    let freeDoubleRooms;
+    let freeSuiteRooms;
+
+    const error = new Error();
+
+    if (rooms.single && rooms.single > 0) {
+        const singleRooms = await db.get({
+            TableName: "bonzaiRooms",
+            Key: {
+                roomType: "single",
+            },
+        });
+
+        freeSingleRooms = singleRooms.Item.available;
+        if (rooms.single > freeSingleRooms) {
+            error.message = `You are trying to book ${rooms.single} rooms but there are only ${freeSingleRooms} availiable`;
+            throw error;
+        }
+    }
+    if (rooms.double && rooms.double > 0) {
+        const doubleRooms = await db.get({
+            TableName: "bonzaiRooms",
+            Key: {
+                roomType: "double",
+            },
+        });
+        freeDoubleRooms = doubleRooms.Item.available;
+        if (rooms.single > freeDoubleRooms) {
+            error.message = `You are trying to book ${rooms.double} rooms but there are only ${freeDoubleRooms} availiable`;
+            throw error;
+        }
+    }
+
+    if (rooms.suite) {
+        const suiteRooms = await db.get({
+            TableName: "bonzaiRooms",
+            Key: {
+                roomType: "suite",
+            },
+        });
+        freeSuiteRooms = suiteRooms.Item.available;
+        if (rooms.single > freeSuiteRooms) {
+            error.message = `You are trying to book ${rooms.suite} rooms but there are only ${freeSuiteRooms} availiable`;
+            throw error;
+        }
+    }
+};
 export { bizLogic };
